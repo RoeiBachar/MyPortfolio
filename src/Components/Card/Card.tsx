8419a44586c45b0eedcc5b7b3059c3b0173e2989
RoeiBachar <roeibachar972@gmail.com>
2023-05-04 15:37:56 +0300

    fix some issues

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -8,8 +8,14 @@ function Card(props: cardInterface): JSX.Element {
     <div className="Card">
       <div id="cardContainer">
         <div id="skils">
-          {props.skils.map((item) => (
-            <p>⭐{item}</p>
+          {props.skils.map((item, index) => (
+            <span
+              style={{
+              }}
+              key={index}
+            >
+              ⭐{item}
+            </span>
           ))}
         </div>
         <div id="mainCardContainer">
@@ -20,13 +26,20 @@ function Card(props: cardInterface): JSX.Element {
             <p>{props.info}</p>
           </div>
           <div id="photoCard">
-            <img draggable="false"className="pc" src={props.imagePc} />
-            <img draggable="false"className="phone" src={props.imageIphone} height={650} />
+            <img draggable="false" className="pc" src={props.imagePc} />
+            <img
+              draggable="false"
+              className="phone"
+              src={props.imageIphone}
+              height={650}
+            />
           </div>
         </div>
 
         <div id="links">
-          <a href={props.gitHubLink} target="_blank"><img draggable="false" src={git} width={300} /></a>
+          <a href={props.gitHubLink} target="_blank">
+            <img draggable="false" src={git} width={300} />
+          </a>
           <img draggable="false" src={website} width={300} />
         </div>
       </div>
